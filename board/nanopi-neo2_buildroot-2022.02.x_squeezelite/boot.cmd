6fc316d457a15fea4b44e4453c2a0c5d9445007d
bz31 <yangtze31@gmail.com>
2022-07-29 12:37:39 +0200

    nanopi-neo2_buildroot-2022.02.x_squeezelite

---
+++ board/nanopi-neo2_buildroot-2022.02.x_squeezelite/boot.cmd
@@ -0,0 +1,6 @@
+setenv bootargs root=/dev/mmcblk0p2 rootwait isolcpus=2,3
+
+fatload mmc 0 $kernel_addr_r Image
+fatload mmc 0 $fdt_addr_r sun50i-h5-nanopi-neo2.dtb
+
+booti $kernel_addr_r - $fdt_addr_r
