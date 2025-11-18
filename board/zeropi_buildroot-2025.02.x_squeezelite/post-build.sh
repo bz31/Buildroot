965ef567e797924e8a0ba6203cf66d1b1a78f36b
bz31 <yangtze31@gmail.com>
2025-11-18 22:11:58 +0100

    Adapt ZeroPi setup for extlinux.conf boot

--- board/zeropi_buildroot-2025.02.x_squeezelite/post-build.sh
+++ board/zeropi_buildroot-2025.02.x_squeezelite/post-build.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-BOARD_DIR="$( dirname "${0}" )"
+BOARD_DIR="$(dirname "$0")"
 
-mv $TARGET_DIR/etc/network/interfaces $TARGET_DIR/etc/network/interfaces.orig
-cp $BOARD_DIR/interfaces $TARGET_DIR/etc/network
-cp $BOARD_DIR/S35macaddress $TARGET_DIR/etc/init.d
-cp $BOARD_DIR/S45ntpdate $TARGET_DIR/etc/init.d
-cp $BOARD_DIR/S95squeezelite $TARGET_DIR/etc/init.d
-mv $TARGET_DIR/etc/shairport-sync.conf $TARGET_DIR/etc/shairport-sync.conf.orig
-cp $BOARD_DIR/shairport-sync.conf $TARGET_DIR/etc
-cp $BOARD_DIR/htoprc $TARGET_DIR/etc
+install -m 0644 -D "$BOARD_DIR"/extlinux.conf "$TARGET_DIR"/boot/extlinux/extlinux.conf
+
+mv $TARGET_DIR/etc/network/interfaces $TARGET_DIR/root/interfaces.orig
+install -m 0644 -D $BOARD_DIR/interfaces $TARGET_DIR/etc/network/interfaces
+#mv $TARGET_DIR/etc/init.d/S40network $TARGET_DIR/root/S40network.orig
+#install -m 0755 -D $BOARD_DIR/S40network $TARGET_DIR/etc/init.d/S40network
+install -m 0755 -D $BOARD_DIR/S35macaddress $TARGET_DIR/etc/init.d/S35macaddress
+install -m 0755 -D $BOARD_DIR/S45ntpdate $TARGET_DIR/etc/init.d/S45ntpdate
+install -m 0755 -D $BOARD_DIR/S95squeezelite $TARGET_DIR/etc/init.d/S95squeezelite
+mv $TARGET_DIR/etc/shairport-sync.conf $TARGET_DIR/root/shairport-sync.conf.orig
+install -m 0644 -D $BOARD_DIR/shairport-sync.conf $TARGET_DIR/etc/shairport-sync.conf
+install -m 0644 -D $BOARD_DIR/htoprc $TARGET_DIR/etc/htoprc
